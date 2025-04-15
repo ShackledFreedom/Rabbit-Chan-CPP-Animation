e5d98e8ffd9dfa7f3ad673e015809241c0026c43
ShackledFreedom <shackledfreedommedia@gmail.com>
2025-04-14 19:47:56 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+int main()
+{
+    system("ffplay -v 0 -nodisp -loop 0 fnaf.opus &");
+    while(true)
+    {
+        std::cout << std::endl;
+        std::cout << "                        /    \\-----/      \\                           " << std::endl;
+        std::cout << "                      /                      \\                 \e[0;103m\e[1;30mWhat the fuck\e[0m           " << std::endl;
+        std::cout << "                     |                        |                 \e[0;103m\e[1;30mis a computer?\e[0m        " << std::endl;
+        std::cout << "                    /         /--   -- \\       \\                    " << std::endl;
+        std::cout << "                   |         /\e[0;31m()\e[0m_) (_\e[0;31m()\e[0m_\\       |         " << std::endl;
+        std::cout << "                   |        /    \\ /     \\      |                                   " << std::endl;
+        std::cout << "                   |       /      |       \\     |                      " << std::endl;
+        std::cout << "                  /       / .. (_/\e[0;31m_\e[0m\\_) ..  \\     \\                     " << std::endl;
+        std::cout << "                 |       /   ..       ..    \\     |                  " << std::endl;
+        std::cout << "                 |       /   ..       ..    \\     |                 " << std::endl;
+        std::cout << "                 |       ..               ..       |                 " << std::endl;
+        std::cout << "                /   \e[1;95m..   \\\e[0m                 \e[1;95m/   ..\e[0m   \\                   " << std::endl;
+        std::cout << "               |  \e[1;95m/       \\\e[0m               \e[1;95m/       \\\e[0m  |                 " << std::endl;
+        std::cout << "               \e[1;95m/           \\             /           \\\e[0m                    " << std::endl;
+        std::cout << "              \e[1;95m/        o     \\\e[0m    |     \e[1;95m/    o        \\                   " << std::endl;
+        std::cout << "             /        o       \\\e[0m   |   \e[1;95m/       o        \\                   " << std::endl;
+        std::cout << "             /      o           \\\e[0m | \e[1;95m/           o      \\                 " << std::endl;
+        std::cout << "            /      o              v              o      \\                 " << std::endl;
+        std::cout << "            /      o                             o      \\                  " << std::endl;
+        std::cout << "           /       o                             o       \\                 " << std::endl;
+        std::cout << "          /         o                           o         \\                " << std::endl;
+        std::cout << "          .....       o          ooo          o       .....               " << std::endl;
+        std::cout << "               .....    ooooooooo   ooooooooo    .....                    " << std::endl;
+        std::cout << "               \e[0m/\e[1;95m    ...                       ...    \e[0m\\                    " << std::endl;
+        std::cout << "               /\e[1;95m      .                       .      \e[0m\\                    " << std::endl;
+        std::cout << "              /\e[1;95m      .                         .       \e[0m\\                  " << std::endl;
+        std::cout << "            /\e[1;95m       .                           .        \e[0m\\                " << std::endl;
+        std::cout << "           /\e[1;95m      ..    \\                   /     ..      \e[0m\\              " << std::endl;
+        std::cout << "          /\e[1;95m     ..       \\                 /        ..     \e[0m\\               " << std::endl;
+        std::cout << "         /\e[1;95m     ..         \\               /          ..     \e[0m\\              " << std::endl;
+        std::cout << "        /\e[1;95m     ..            \\           /             ..     \e[0m\\              " << std::endl;
+        std::cout << "     //     /\e[1;95m ..                                      .. \e[0m\\     \\\\            " << std::endl;
+        std::cout << "   //   ////\e[1;95m  ..                                      ..  \e[0m\\\\\\\\   \\\\         " << std::endl;
+        std::cout << "  //////\e[1;95m      ..             ...........              ..      \e[0m\\\\\\\\\\\\       " << std::endl;
+        std::cout << "               \e[1;95m..............     \e[0m.\e[1;95m     ...............\e[0m                   " << std::endl;
+        std::cout << "               ..                 .                  ..                   " << std::endl;
+        std::cout << "               ..                 .                  ..                   " << std::endl;
+
+
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
+        system("clear");
+
+        std::cout << std::endl << std::endl;
+        std::cout << "                          ___        ___                       What the fuck      " << std::endl;
+        std::cout << "                        /    \\-----/      \\                     is a computer?        " << std::endl;
+        std::cout << "                      /                      \\                            " << std::endl;
+        std::cout << "                     |                        |                         " << std::endl;
+        std::cout << "                    /         /--   -- \\       \\                    " << std::endl;
+        std::cout << "                   |         /\e[0;31m()\e[0m_) (_\e[0;31m()\e[0m_\\       |         " << std::endl;
+        std::cout << "                   |        /    \\ /     \\      |                                   " << std::endl;
+        std::cout << "                   |       /      |       \\     |                      " << std::endl;
+        std::cout << "                  /       / .. (_/ \\_) ..  \\     \\                     " << std::endl;
+        std::cout << "                 |       /   ..       ..    \\     |                  " << std::endl;
+        std::cout << "                 |       /   ..       ..    \\     |                 " << std::endl;
+        std::cout << "                 |       ..               ..       |                 " << std::endl;
+        std::cout << "                /   \e[1;95m..   \\\e[0m                 \e[1;95m/   ..\e[0m   \\                   " << std::endl;
+        std::cout << "               |  \e[1;95m/       \\\e[0m               \e[1;95m/       \\\e[0m  |                 " << std::endl;
+        std::cout << "               \e[1;95m/           \\             /           \\\e[0m                    " << std::endl;
+        std::cout << "              \e[1;95m/        o     \\\e[0m    |     \e[1;95m/    o        \\                   " << std::endl;
+        std::cout << "             /        o       \\\e[0m   |   \e[1;95m/       o        \\                   " << std::endl;
+        std::cout << "             /      o           \\\e[0m | \e[1;95m/           o      \\                 " << std::endl;
+        std::cout << "            /      o              v              o      \\                 " << std::endl;
+        std::cout << "            /      o                             o      \\                  " << std::endl;
+        std::cout << "           /       o                             o       \\                 " << std::endl;
+        std::cout << "          /         o                           o         \\                " << std::endl;
+        std::cout << "          .....       o          ooo          o       .....               " << std::endl;
+        std::cout << "               .....    ooooooooo   ooooooooo    .....                    " << std::endl;
+        std::cout << "               \e[0m/\e[1;95m    ...                       ...    \e[0m\\                    " << std::endl;
+        std::cout << "               /\e[1;95m      .                       .      \e[0m\\                    " << std::endl;
+        std::cout << "              /\e[1;95m      .                         .       \e[0m\\                  " << std::endl;
+        std::cout << "            /\e[1;95m       .                           .        \e[0m\\                " << std::endl;
+        std::cout << "           /\e[1;95m      ..    \\                   /     ..      \e[0m\\              " << std::endl;
+        std::cout << "          /\e[1;95m     ..       \\                 /        ..     \e[0m\\               " << std::endl;
+        std::cout << "         /\e[1;95m     ..         \\               /          ..     \e[0m\\              " << std::endl;
+        std::cout << "      /      /\e[1;95m..            \\           /             ..\e[0m\\     \\              " << std::endl;
+        std::cout << "  ////     /\e[1;95m  ..                                      ..  \e[0m\\     \\\\\\\\            " << std::endl;
+        std::cout << "///////////\e[1;95m   ..                                      ..   \e[0m\\\\\\\\\\\\\\\\\\\\\\         " << std::endl;
+        std::cout << "        \e[1;95m      ..             ...........              ..       \e[0m       " << std::endl;
+        std::cout << "               \e[1;95m..............     \e[0m.\e[1;95m     ...............\e[0m                   " << std::endl;
+        std::cout << "               ..                 .                  ..                   " << std::endl;
+        std::cout << "               ..                 .                  ..                   " << std::endl;
+
+
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
+        system("clear");
+    }
+    return 0;
+}
